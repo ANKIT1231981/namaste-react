3350fee232b7c222bda5b7bb7fc36728b805be73
ANKIT1231981 <solankitsi121@gmail.com>
2026-03-31 02:14:35 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,63 @@
+const parentDiv = React.createElement("div", { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" },
+      [
+        React.createElement("h1", {}, "Hey, I am Ankit"),
+        React.createElement("h2", {}, "Hey, I am Rohan")
+      ]),
+
+    React.createElement("div", { id: "child2" },
+      [
+        React.createElement("h1", {}, "Hey, I am Ankit"),
+        React.createElement("h2", {}, "Hey, I am Rohan")
+      ])
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parentDiv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let root = document.getElementById("root");
+
+// let heading = document.createElement("h1");
+
+// heading.innerHTML = "hey, I am Ankit";
+
+// root.appendChild(heading);
